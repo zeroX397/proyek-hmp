090db27f1a8b9f9949f7462b65ae4328b9f1d2c8
Christopher Pengalilla <christopher.pengalilla@gmail.com>
2024-10-14 18:43:07 +0700

    Adding Animation to Member Profile Picture

--- src/app/what-we-play/team/team-member/team-member.page.ts
+++ src/app/what-we-play/team/team-member/team-member.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GameService, TeamMember, Team } from 'src/app/game.service';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-team-member',
@@ -11,26 +12,48 @@ export class TeamMemberPage implements OnInit {
   members: TeamMember[] = [];
   teamId!: number;
   teamName!: string;
-  gameId!: number; 
+  gameId!: number;
 
   constructor(
-    private route: ActivatedRoute, 
-    private gameService: GameService) {}
+    private route: ActivatedRoute,
+    private animationCtrl: AnimationController,
+    private gameService: GameService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.teamId = +params['id'];  
-      this.members = this.gameService.getTeamMembers(this.teamId);  
-      
+      this.teamId = +params['id'];
+      this.members = this.gameService.getTeamMembers(this.teamId);
+
       const team = this.gameService.getTeams().find(t => t.id === this.teamId);
       if (team) {
         this.teamName = team.name;
-        this.gameId = team.gameId; 
+        this.gameId = team.gameId;
       }
     });
   }
 
   getGameImage(): string {
     return this.gameService.getGameImage(this.gameId);
+  }
+
+  ionViewDidEnter() {
+    this.shrinkGrowAvatar();
+  }
+
+  shrinkGrowAvatar() {
+    const avatarElements = document.querySelectorAll('#avatar') as NodeListOf<HTMLElement>;
+    avatarElements.forEach(avatarElement => {
+      const animation = this.animationCtrl
+        .create()
+        .addElement(avatarElement)
+        .duration(5000) // Duration in ms
+        .iterations(3) // Do animation 3 times
+        .keyframes([
+          { offset: 0, transform: 'scale(1)' }, // Start with normal size
+          { offset: 0.5, transform: 'scale(1.5)' }, // Grow to 1.2x size
+          { offset: 1, transform: 'scale(1)' }, // Back to normal size
+        ]);
+      animation.play();
+    });
   }
 }
